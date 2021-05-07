65a57900a675a6b5cc14646461bbe4a2ea1cd61a
codadept <info.pratikmajumdar@gmail.com>
2021-05-07 06:05:39 +0530

    rearranging files

---
+++ js/app.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+    $('.list').click(function(){
+        const value = $(this).attr('data-filter');
+        if(value == 'all'){
+            $('.itemBox').show('1000');
+        }
+        else{
+            $('.itemBox').not('.'+value).hide('1000');
+            $('.itemBox').filter('.'+value).show('1000');
+        }
+    })
+
+    $('.list').click(function(){
+        $(this).addClass('active').siblings().removeClass('active');
+    })
+})
